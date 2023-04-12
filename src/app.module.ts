0df69945231fdb0b2005a760f8ee6200f3819e71
Flamierdx <nikitadzhurinskiy06@gmail.com>
2023-04-12 22:34:28 +0300

    (App): MongoDB container created and connected from Mongoose Module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseConfigService } from './shared/config/mongoose.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useClass: MongooseConfigService,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/shared/config/mongoose.config.ts
@@ -0,0 +1,21 @@
+import { MongooseModuleOptions, MongooseOptionsFactory } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class MongooseConfigService implements MongooseOptionsFactory {
+  constructor(private readonly config: ConfigService) {}
+
+  createMongooseOptions(): MongooseModuleOptions {
+    const user = this.config.get<string>('MONGO_USER');
+    const password = this.config.get<string>('MONGO_PASSWORD');
+    const host = this.config.get<string>('MONGO_HOST');
+    const port = this.config.get<number>('MONGO_PORT');
+    const databaseName = this.config.get<string>('MONGO_DB');
+
+    return {
+      uri: `mongodb://${user}:${password}@${host}:${port}/${databaseName}`,
+      autoIndex: true,
+    };
+  }
+}
